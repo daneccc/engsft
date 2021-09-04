b5bb58863650aee712437cf488d136f520a5e018
Carlos Victor Martins da Silva viktsys <viktsys@gmail.com>
2021-09-04 19:11:50 -0300

    [BACKEND] Definindo as rotas da versão v1.0 da API (incompleto)

--- backend/src/routes/accountRouter.ts
+++ backend/src/routes/accountRouter.ts
@@ -2,4 +2,20 @@ import { Router, Request, Response } from "express"
 
 const accountRouter = Router()
 
+accountRouter.get('/info', (req: Request, res: Response) => {
+    res.status(200).send({message: ""})
+})
+
+accountRouter.get('/settings', (req: Request, res: Response) => {
+    res.status(200).send({message: ""})
+})
+
+accountRouter.post('/info', (req: Request, res: Response) => {
+    res.status(200).send({message: ""})
+})
+
+accountRouter.post('/settings', (req: Request, res: Response) => {
+    res.status(200).send({message: ""})
+})
+
 export default accountRouter
--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -2,9 +2,20 @@ import { Router, Request, Response } from "express"
 
 const authRouter = Router()
 
-authRouter.get('/', (req: Request, res: Response) => {
+authRouter.post('/login', (req: Request, res: Response) => {
     res.status(200).send({message: ""})
 })
 
+authRouter.post('/register', (req: Request, res: Response) => {
+    res.status(200).send({message: ""})
+})
+
+authRouter.post('/renew', (req: Request, res: Response) => {
+    res.status(200).send({message: ""})
+})
+
+authRouter.post('/lost-password', (req: Request, res: Response) => {
+    res.status(200).send({message: ""})
+})
 
 export default authRouter;
