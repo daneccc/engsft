20edcf0d78b94c03311049692f038707f5079a74
Carlos Victor Martins da Silva viktsys <viktsys@gmail.com>
2021-09-04 18:59:51 -0300

    [BACKEND] Definindo as interfaces User, Backup e PublicKey.

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,3 +1,14 @@
-export class User {
-    
+import {PublicKey} from "@models/PublicKey"
+
+interface User {
+    uid: String
+    email: String
+    firstName: String
+    lastName: String
+    pHash: String
+    pSalt: String,
+    createdAt: Date,
+    publicKey: PublicKey
 }
+
+export {User}
